dde1c55b9b5c4e941cc6776660dc74020177f585
hm <hamidreza.mousavi@yahoo.com>
2021-11-14 16:22:42 +0330

    initia render page1

--- render.py
+++ render.py
@@ -1,35 +1,184 @@
+from os import replace
+from enum import Enum
 from PIL import Image, ImageDraw, ImageFont
 import arabic_reshaper
 from bidi.algorithm import get_display
-from data import currency
-from tgju import get_data
+from flask.templating import render_template
+from data import Index
+from fileoperation import getDataOnFile
 
-data = get_data()
+
+def setColor(diff = None):
+    RED = "#ff0000"
+    GREEN = "#00ff00"
+    WHITE = "#ffffff"
+    if diff == None:
+        color = WHITE
+    elif (diff == '0') or (diff == '0.00'):
+        color = WHITE 
+    elif diff.find('-')==0:
+        color = RED
+    else:
+        color = GREEN
+    return color
+
+def strDiff(str1, str2):
+    try:
+        str1=str1.replace(',','')
+        str2=str2.replace(',','')
+        try:
+            str1 = int(str1)
+        except ValueError:
+            str1 = float(str1)
+        try:
+            str2 = int(str2)
+        except ValueError:
+            str2 = float(str2)
+        resualt = str1 - str2
+    except:
+        return None
+    if isinstance(resualt,float):
+        resualt = f'{resualt:0.2f}'
+    return str(resualt)
+
+    
+ 
+
 
 def prepare_text(text):
     reshaped_text = arabic_reshaper.reshape(text)    
     bidi_text = get_display(reshaped_text)
     return bidi_text
 
-def prepare_image(data):
-
-    img = Image.open('background.jpg')
-   
-    #reg = (50,20,100,40)
-    #cut = img.crop(reg)
-    #img.paste(pimg,(50,20))
-    #img = img.transpose(Image.ROTATE_90)
-    #out = img.point(lambda i: i * 3)
-    #print(img.getbands())
-    #im = Image.new("RGB", (200, 200), "white")
-    # d.line(((0, 100), (200, 100)), "gray")
-   # d.line(((100, 0), (100, 200)), "gray")
+def renderPage1():
+    
+    indexs_list = getDataOnFile('indexarchive.json')
+    last_indexs = indexs_list[-1]
+    prv_indexs = indexs_list[-2]
+    img = Image.open('page1_tepl.jpg')
+    d = ImageDraw.Draw(img)  
+    
+    color = setColor()
     font = ImageFont.truetype("BNAZANIN_0.ttf", 48)
+    text = prepare_text(last_indexs['dollar_rl'].name)
+    d.text((620, 90), text, fill=color, anchor="rm", font=font)
     
+    diff = strDiff(last_indexs['dollar_rl'].value, prv_indexs['dollar_rl'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 90), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['dollar_rl'].value)
+    d.text((170, 90), text, fill=color, anchor="mm", font=font)
     
+    #******************************
+    color = setColor()
+    font = ImageFont.truetype("BNAZANIN_0.ttf", 48)
+    text = prepare_text(last_indexs['eur'].name)
+    d.text((620, 160), text, fill=color, anchor="rm", font=font)
     
-    d = ImageDraw.Draw(img)
-   
-    d.text((100, 100), bidi_text, fill="green", anchor="ms", font=font)
+    diff = strDiff(last_indexs['eur'].value, prv_indexs['eur'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 160), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['eur'].value)
+    d.text((170, 160), text, fill=color, anchor="mm", font=font)
+    
+#******************************
+    color = setColor()
+    font = ImageFont.truetype("BNAZANIN_0.ttf", 48)
+    text = prepare_text(last_indexs['aed'].name)
+    d.text((620, 240), text, fill=color, anchor="rm", font=font)
+    
+    diff = strDiff(last_indexs['aed'].value, prv_indexs['aed'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 240), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['aed'].value)
+    d.text((170, 240), text, fill=color, anchor="mm", font=font)
 
-    img.show()
+#******************************
+    color = setColor()
+    font = ImageFont.truetype("BNAZANIN_0.ttf", 48)
+    text = prepare_text(last_indexs['try'].name)
+    d.text((620, 320), text, fill=color, anchor="rm", font=font)
+    
+    diff = strDiff(last_indexs['try'].value, prv_indexs['try'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 320), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['try'].value)
+    d.text((170, 320), text, fill=color, anchor="mm", font=font)
+    
+#******************************
+    color = setColor()
+    font = ImageFont.truetype("BNAZANIN_0.ttf", 42)
+    text = prepare_text(last_indexs['geram18'].name)
+    d.text((620, 400), text, fill=color, anchor="rm", font=font)
+    
+    diff = strDiff(last_indexs['geram18'].value, prv_indexs['geram18'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 400), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['geram18'].value)
+    d.text((170, 400), text, fill=color, anchor="mm", font=font)
+       
+    
+#******************************
+    color = setColor()
+    font = ImageFont.truetype("BNAZANIN_0.ttf", 42)
+    text = prepare_text(last_indexs['sekee'].name)
+    d.text((620, 480), text, fill=color, anchor="rm", font=font)
+    
+    diff = strDiff(last_indexs['sekee'].value, prv_indexs['sekee'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 480), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['sekee'].value)
+    d.text((170, 480), text, fill=color, anchor="mm", font=font)
+        
+#******************************
+    color = setColor()
+    font = ImageFont.truetype("BNAZANIN_0.ttf", 42)
+    text = prepare_text(last_indexs['btc'].name)
+    d.text((620, 560), text, fill=color, anchor="rm", font=font)
+    
+    diff = strDiff(last_indexs['btc'].value, prv_indexs['btc'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 560), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['btc'].value)
+    d.text((170, 560), text, fill=color, anchor="mm", font=font)
+#******************************
+    color = setColor()
+    font = ImageFont.truetype("BNAZANIN_0.ttf", 42)
+    text = prepare_text(last_indexs['bourcind'].name)
+    d.text((700, 640), text, fill=color, anchor="rm", font=font)
+    
+    diff = strDiff(last_indexs['bourcind'].value, prv_indexs['bourcind'].value)
+    text = prepare_text(diff)
+        
+    color = setColor(diff)
+    d.text((370, 640), text, fill=color, anchor="mm", font=font)
+    text = prepare_text(last_indexs['bourcind'].value)
+    d.text((170, 640), text, fill=color, anchor="mm", font=font)
+                  
+    
+    img.save('page1.jpg')
+    #img.show()
+
+
+
+
+#font = ImageFont.truetype(font_path, size=600)
+#draw = ImageDraw.Draw(img)
+#width_text, height_text = draw.textsize(text, font)
+#
+#offset_x, offset_y = font.getoffset(text)
+#width_text += offset_x
